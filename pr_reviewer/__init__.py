fcffd814f45d13919d9e326a85d70c0580ff710f
George Song <george@gsong.dev>
2025-12-31 17:22:26 -0800

    refactor: improve module exports in __init__.py files
    - Update pr_reviewer/__init__.py to export state, app, index
    - Update pr_reviewer/services/__init__.py to export all service functions

---
+++ pr_reviewer/__init__.py
@@ -0,0 +1,4 @@
+from pr_reviewer import state
+from pr_reviewer.pr_reviewer import app, index
+
+__all__ = ["state", "app", "index"]
--- pr_reviewer/services/__init__.py
+++ pr_reviewer/services/__init__.py
@@ -1,1 +1,6 @@
 """Services for PR Reviewer."""
+
+from pr_reviewer.services.github import fetch_pr_files, fetch_pr_metadata, parse_pr_url
+from pr_reviewer.services.reviewer import review_diff
+
+__all__ = ["fetch_pr_files", "fetch_pr_metadata", "parse_pr_url", "review_diff"]
